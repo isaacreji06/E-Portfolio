f41d32ef720dcaac7421ce56546f439e3002e6bf
Isaac Reji <isaac.reji@kalvium.community>
2025-04-13 17:18:58 +0530

    completed

--- Frontend/src/Components/LandingPage.jsx
+++ Frontend/src/Components/LandingPage.jsx
@@ -1,10 +1,11 @@
-import heroImage from "../assets/heroImage.png"
 import { Link } from "react-router-dom";
+import image from "../assets/RebeccaReji.jpg"
 
 const LandingPage = () => {
   return (
     <section className="bg-gray-50 h-screen flex items-center justify-center">
       <div className="max-w-4xl mx-auto text-center px-6">
+        <img src={image} alt="Rebecca Reji" className="rounded-full w-64 mb-4 ml-72 " />
         <h1 className="text-4xl md:text-5xl font-extrabold text-gray-800 mb-4">
           Hi, I'm Rebecca Reji
         </h1>
